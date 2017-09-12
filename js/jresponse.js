1eba95dea04956347e04daf23e4e69965f43a8b2
Auhill <bewill@yeah.net>
2017-09-12 20:14:05 +0800

    minor bugs fixed

--- js/jresponse.js
+++ js/jresponse.js
@@ -15,16 +15,18 @@ serialport.list((err, ports) => {
     document.getElementById('error').textContent = 'No ports discovered'
 }
 
-//this is for testing
+//this function is for testing
 $("#e1label").click(function() {
     alert( "Handler for btndiv1 .click() called." );
     $("#e1label").text("clicked!");
 });  
+
+//on button click
 $("button").click(function(e) {
 	var b_id = $(e.target).attr('id'); 
 	// alert(b_id)
-	byte1 = get_byte1(b_id);
-	message = get_message(byte1);
+	let byte1 = get_byte1(b_id);
+	let message = get_message(byte1);
 	alert(message.length)
 	ports.forEach(port => message.write(port))
 });
@@ -33,17 +35,17 @@ setInterval(send_clock(),500)
 
 function get_message(byte1){
 	//construct Bytes for Click Events
-	byte0 = new Buffer('11','hex');
-	byte234 = new Buffer('00','hex');
-	byte6 = new Buffer('0a','hex');
-	byte5 = crc.crc8(byte0,byte1,byte234,byte234,byte234);
-	message = Buffer.concat([byte0,byte1,byte234,byte234,byte234,byte5,byte6]);
+	const byte0 = new Buffer('11','hex');
+	const byte234 = new Buffer('00','hex');
+	const byte6 = new Buffer('0a','hex');
+	let byte5 = crc.crc8(byte0,byte1,byte234,byte234,byte234);
+	let message = Buffer.concat([byte0,byte1,byte234,byte234,byte234,byte5,byte6]);
 	return message;
 }
 
 //Get byte1
 function get_byte1(b_id){
-	id_array = [
+	const id_array = [
 	'e1po','e1no','e1pm','e1nm',
 	'e2po','e2no','e2pm','e2nm',
 	's1on','s1off','s2on','s2off','s3on','s3off',
@@ -51,12 +53,13 @@ function get_byte1(b_id){
 	'd1p','d1n','d2p','d2n','d3p','d3n','d4p','d4n','d5p','d5n',
 	'd1r','d2r','d3r','d4r','d5r'
 	]
-	id_index = id_array.indexOf(b_id)
+	let id_index = id_array.indexOf(b_id)
 
-	label_list = [
+	const label_list = [
 	'l_e1','l_e2','l_s1','l_s2','l_s3','l_d1','l_d2','l_d3','l_d4','ld5'
 	]
 
+	//change the label text
 	if (id_index >= 0){
 		if ((id_index<4)||(id_index==14)){
 			label_index = 0
@@ -83,20 +86,20 @@ function get_byte1(b_id){
 		}
 	$("#"+label_list[label_index]).text(id_array[id_index]+"clicked!");
 
-
-		event_order = id_index+1;
-		byte1 = new Buffer(event_order.toString(),'hex');
+		let event_order = id_index+1;
+		let byte1 = new Buffer(event_order.toString(),'hex');
 	}else{
 		alert('id not found!');
-		byte1 = new Buffer('00','hex');
+		let byte1 = new Buffer('00','hex');
 	}
 	return byte1;
 }
 
+//clock
 function send_clock(){
-	byte0 = new Buffer('13','hex');
-	byte6 = new Buffer('0a','hex');
-	byte15 = new Buffer('00','hex');	
-	message = Buffer.concat([byte0,byte15,byte15,byte15,byte15,byte15,byte6]);
+	const byte0 = new Buffer('13','hex');
+	const byte6 = new Buffer('0a','hex');
+	const byte15 = new Buffer('00','hex');	
+	const message = Buffer.concat([byte0,byte15,byte15,byte15,byte15,byte15,byte6]);
 	ports.forEach(port => message.write(port))
 }
